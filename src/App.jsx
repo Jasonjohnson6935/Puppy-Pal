c608d0bb06a92d61d5f4ae20d1f454e976f02a09
Jasonjohnson6935 <jason.johnson6935@gmail.com>
2023-12-17 21:09:44 -0600

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
- 
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>{ 
+   puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   })
+}
       </div>
     </>
   )
